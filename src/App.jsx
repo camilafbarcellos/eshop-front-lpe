d6ea5903cdcd73c28a9ffcb2f4bb6098f473f6d7
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-04-09 11:26:18 -0300

    aula 09/04/24: segurança JWT

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,54 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./componentes/Home";
-import Menu from "./componentes/Menu";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './componentes/Home';
+import MenuPrivado from './componentes/MenuPrivado';
+import MenuPublico from './componentes/MenuPublico';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@popperjs/core/dist/cjs/popper.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import Categoria from "./componentes/telas/categoria/Categoria";
-import Produto from "./componentes/telas/produto/Produto";
+import Categoria from './componentes/telas/categoria/Categoria';
+import Produto from './componentes/telas/produto/Produto';
+import Login from './componentes/telas/login/Login';
 
 const router = createBrowserRouter([
   {
-    path : "/",
-    element : <Menu/>,
-    children : [
+    path: '/',
+    element: <MenuPublico />,
+    children: [
       {
-        index : true,
-        element : <Home/>
+        index: true,
+        element: <Home />
       },
       {
-        path : "categorias",
-        element : <Categoria/>
+        path: 'login',
+        element: <Login />
+      }
+    ]
+  },
+  {
+    path: '/privado',
+    element: <MenuPrivado />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'categorias',
+        element: <Categoria />
       }
       ,
       {
-        path : "produtos",
-        element : <Produto/>
-      }      
+        path: 'produtos',
+        element: <Produto />
+      }
     ]
   }
 ])
 
 function App() {
   return (
-      <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   );
 }
 
